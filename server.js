d8cd6c2cb0a4170daaa56f63792786d67076b169
RadoslawSzymansky <radek199@gmail.com>
2019-08-02 21:45:36 +0200

    last

--- server.js
+++ server.js
@@ -1,50 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-
-http.createServer(function (request, response) {
-
-  var filePath = '.' + request.url;
-  if (filePath == './') filePath = './index.html';
-
-  var extname = path.extname(filePath);
-  var contentType = 'text/html';
-  switch (extname) {
-    case '.js':
-      contentType = 'text/javascript';
-      break;
-    case '.css':
-      contentType = 'text/css';
-      break;
-  }
-
-  path.exists(filePath, function (exists) {
-
-    if (exists) {
-      fs.readFile(filePath, function (error, content) {
-        if (error) {
-          response.writeHead(500);
-          response.end();
-        }
-        else {
-          response.writeHead(200, { 'Content-Type': contentType });
-          response.end(content, 'utf-8');
-        }
-      });
-    }
-    else {
-      response.writeHead(404);
-      response.end();
-    }
-  });
-
-}).listen(process.env.PORT || 5000)
-
-console.log('Server running at http://127.0.0.1:5000/');  
-
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://user:admin123@cluster0-ahxwi.mongodb.net/test?retryWrites=true&w=majority');
 
