c6920e04fac3e0744c46912495fa93c354314c5c
RadoslawSzymansky <radek199@gmail.com>
2019-08-02 21:39:47 +0200

    last

--- server.js
+++ server.js
@@ -8,9 +8,39 @@ var path = require('path');
 http.createServer(function (request, response) {
 
   var filePath = '.' + request.url;
-  if (filePath == './')
-    filePath = './index.html';
-  
+  if (filePath == './') filePath = './index.html';
+
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+  }
+
+  path.exists(filePath, function (exists) {
+
+    if (exists) {
+      fs.readFile(filePath, function (error, content) {
+        if (error) {
+          response.writeHead(500);
+          response.end();
+        }
+        else {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+        }
+      });
+    }
+    else {
+      response.writeHead(404);
+      response.end();
+    }
+  });
+
 }).listen(process.env.PORT || 5000)
 
 console.log('Server running at http://127.0.0.1:5000/');  
